43852a165855a6dba50a98d6bf31343276d44681
sebasca9 <scastanoa1@eafit.edu.co>
2023-03-28 14:08:09 -0500

    Codigo base con el metodo Salida en comentario

---
+++ Residuos.java
@@ -0,0 +1,139 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Residuos extends JPanel implements ItemListener, ActionListener{
+    private JLabel labelResiduo;
+    private JComboBox<String> opResiduos;
+    private JLabel labelNombre;
+    private JTextField nombre;
+    private JLabel labelDocumento;
+    private JTextField documento;
+    private JTextArea textField;
+    private JButton bSalida;
+    private JTextArea salida;
+    
+
+    public Residuos() {
+
+        //construct components
+        labelResiduo = new JLabel ("Tipo de Residuo");
+        opResiduos = new JComboBox <String>();
+        labelNombre = new JLabel ("Nombre");
+        nombre = new JTextField (5);
+        labelDocumento = new JLabel ("Documento");
+        documento = new JTextField (5);
+        textField = new JTextArea (5, 5);
+        bSalida = new JButton ("Cerrar");
+
+        //set components properties
+        opResiduos.setToolTipText ("Selecciona el tipo de residuo que quieras botar.");
+        textField.setEnabled (false);
+        bSalida.setToolTipText ("Oprime este boton cuando quieras salir del programa.");
+
+        //adjust size and set layout
+        setPreferredSize (new Dimension (784, 461));
+        setLayout (null);
+
+        //add components
+        add (labelResiduo);
+        add (opResiduos);
+        opResiduos.addItemListener(this);
+        opResiduos.addItem("vacio");
+        opResiduos.addItem("Organico");
+        opResiduos.addItem("Inorganico");
+        opResiduos.addItem("Reciclable");
+        add (labelNombre);
+        add (nombre);
+        add (labelDocumento);
+        add (documento);
+        add (textField);
+        add (bSalida);
+        bSalida.addActionListener(this);
+
+        //set component bounds (only needed by Absolute Positioning)
+        labelResiduo.setBounds (190, 90, 100, 25);
+        opResiduos.setBounds (190, 120, 100, 25);
+        labelNombre.setBounds (325, 90, 100, 25);
+        nombre.setBounds (325, 120, 100, 25);
+        labelDocumento.setBounds (460, 90, 100, 25);
+        documento.setBounds (460, 120, 100, 25);
+        textField.setBounds (190, 170, 370, 120);
+        bSalida.setBounds (460, 305, 100, 25);
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        if (e.getSource()==opResiduos) {
+            String seleccionado=(String)opResiduos.getSelectedItem();
+            if (seleccionado.equals("Organico")) {
+                String org = Organico();
+                textField.setText(org);
+            } else if (seleccionado.equals("Inorganico")) {
+                String inorg = Inorganico();
+                textField.setText(inorg);
+            } else if (seleccionado.equals("Reciclable")) {
+                String rec = Reciclable();
+                textField.setText(rec);
+            } else {
+                textField.setText("No se ha seleccionado ningun residuo.");
+            }
+        }
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource()==bSalida) {
+            Exit();
+        }
+    }
+
+    public void Menu(){
+        JFrame frame = new JFrame ("Residuos");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add (new Residuos());
+        frame.pack();
+        frame.setVisible (true);
+    }
+
+    public String Organico(){
+        String nom = nombre.getText();
+        String doc = documento.getText();
+        String texto = (nom + ", con identificacion " + doc + ", \ntu residuo es organico. Debe ir en la basura verde.");
+        return texto;
+    }
+
+    public String Inorganico(){
+        String nom = nombre.getText();
+        String doc = documento.getText();
+        String texto = (nom + ", con identificacion " + doc + ", \ntu residuo es inorganico. Debe ir en la basura azul.");
+        return texto;
+    }
+
+    public String Reciclable(){
+        String nom = nombre.getText();
+        String doc = documento.getText();
+        String texto = (nom + ", con identificacion " + doc + ", \ntu residuo es reciclable. Debe ir en la basura gris.");
+        return texto;
+    }
+
+    public void Exit(){
+        salida = new JTextArea(5,5);
+        salida.setEnabled(false);
+        setPreferredSize (new Dimension (784, 461));
+        setLayout (null);
+        add(salida);
+        salida.setBounds(45, 205, 500, 200);
+        System.exit(0);
+    }
+
+    /*public void Salida(){
+        JFrame frame = new JFrame ("Salida");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add (new Residuos());
+        frame.pack();
+        frame.setVisible (true);
+    }*/
+
+    public static void main (String[] args) {
+        (new Residuos()).Menu();
+    }
+}
